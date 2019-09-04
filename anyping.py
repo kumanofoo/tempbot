db17fa6509cd7bd9af0a86d1f0a59adb10da59ff
Kumano Foo <kumanofoo@gmail.com>
2019-09-04 10:08:02 +0900

    Alert delay

--- anyping.py
+++ anyping.py
@@ -47,6 +47,12 @@ class Servers():
             self.interval = 60
         log.info('ping interval: {0:d} sec'.format(self.interval))
 
+        if 'alert_delay' in configuration:
+            self.alert_delay = int(configuration['alert_delay'])
+        else:
+            self.alert_delay = 1
+        log.info('alert delay: {0:d} turn'.format(self.alert_delay))
+
         for server in self.servers.keys():
             prop = self.servers[server]
             if prop['type'] == 'DNS':
@@ -57,26 +63,37 @@ class Servers():
                 raise AnypingError("'type of '{0}' is unknown: {1}".format(
                     server, prop['type']))
 
-            prop['alive'], prop['message'] = prop['server'].is_alive()
+            alive, prop['message'] = prop['server'].is_alive()
+            if alive:
+                prop['alive'] = self.alert_delay
+            else:
+                prop['alive'] = 0
 
     def ping(self):
         log.debug("ping()")
         messages = ''
         for server in self.servers.keys():
             alive, message = self.servers[server]['server'].is_alive()
-            log.debug("%s(%s): %s" % (server, alive, message))
+            log.debug(
+                    "%s(%s:%d): %s" %
+                    (server, alive, self.servers[server]['alive'], message)
+                )
             server_type = self.servers[server]['type']
             if alive:
-                if not self.servers[server]['alive']:
+                if self.servers[server]['alive'] <= 0:
                     messages += "{0} is up\n".format(server)
+                self.servers[server]['alive'] = self.alert_delay
             else:
-                if self.servers[server]['alive']:
+                if self.servers[server]['alive'] >= 0:
+                    self.servers[server]['alive'] -= 1
+
+                if self.servers[server]['alive'] == 0:
                     messages += "{0} ({1}) is down: {2}\n".format(
                         server, server_type, message)
 
-            self.servers[server]['alive'] = alive
             self.servers[server]['message'] = message
 
+        log.debug("messages:", messages)
         return messages
 
     def get_status_of_servers(self):
@@ -92,7 +109,6 @@ class Servers():
                 messages += "{0} ({1}) is down: {2}\n".format(
                     server, server_type, message)
 
-            self.servers[server]['alive'] = alive
             self.servers[server]['message'] = message
 
         return messages

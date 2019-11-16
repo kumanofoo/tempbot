9d07496149de68a6f007bf1b776539c1eea78fb0
Kumano Foo <kumanofoo@gmail.com>
2019-11-16 13:37:40 +0900

    Fix shebang Check configuration files

--- install.sh
+++ install.sh
@@ -5,20 +5,37 @@ install -o root -g root -m 640 anyping.py dnsping.py httping.py /opt/tempbotd
 install -o root -g root -m 640 icmping.py weather.py eventlogger.py /opt/tempbotd
 install -o root -g root -m 750 tempbotd.py /opt/tempbotd
 
-if [ ! -f /opt/tempbotd/anyping.conf ]; then
-    install -o root -g root -m 640 anyping-sample.conf /opt/tempbotd/anyping.conf
+if [ -f /opt/tempbotd/anyping-sample.conf ]; then
+    echo skip install /opt/tempbotd/anyping-sample.conf
+else
+    install -o root -g root -m 640 anyping-sample.conf /opt/tempbotd
 fi
 
-if [ ! -f /etc/default/tempbot ]; then
+if [ -f /etc/default/tempbot ]; then
+    echo skip install /etc/default/tempbot
+else
     install -o root -g root -m 600 tempbot /etc/default
 fi
 
-if [ ! -f /etc/systemd/system/tempbotd.service ]; then
+if [ -f /etc/systemd/system/tempbotd.service ]; then
+    echo skip install /etc/systemd/system/tempbotd.service
+else
     install -o root -g root -m 640 tempbotd.service /etc/systemd/system
 fi
 
-type systemctl &> /dev/null
-if [ $? = 0 ]; then
-    systemctl enable tempbotd
-fi
 
+cat << EOS
+
+Install tempbotd as systemd service
+$ sudo systemctl daemon-reload
+
+Start tempbotd service
+$ sodo systemctl start tempbotd
+
+Check tempbotd service
+$ systemctl status tempbotd
+
+Enable to start tempbotd service on system boot 
+$ sudo systemctl enable tempbotd
+
+EOS
